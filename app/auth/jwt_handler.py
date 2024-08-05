7e85055baea413a7d4f1855102b68025cc90f2e4
laxmiverve <laxminarayana.verve@gmail.com>
2024-08-05 18:53:27 +0530

    authentication

---
+++ app/auth/jwt_handler.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timedelta, timezone
+from jose import JWTError, jwt
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+secret_key = os.getenv("SECRET_KEY")
+hashing_algorithm = os.getenv("ALGORITHM")
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 120
+
+ 
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, secret_key, algorithm = hashing_algorithm)
+    return encoded_jwt
+
+
+def decode_jwt_token(token: str):
+    try:
+        decoded_token = jwt.decode(token, secret_key, algorithms = [hashing_algorithm])
+        email: str = decoded_token.get("sub")
+        return email
+    except JWTError:
+        return None
