18ee21ac39c44c59c8788e8bbebef1cfd9afc6ee
laxmiverve <laxminarayana.verve@gmail.com>
2024-08-05 18:57:44 +0530

    login routes and services

---
+++ app/modules/login/user_login_service.py
@@ -0,0 +1,55 @@
+# from sqlalchemy.orm import Session
+# from app.hashing.password_hash import Hash
+# from app.auth.jwt_handler import create_access_token
+# from app.models.user_model import UserModel
+
+
+# def login_user(email: str, password: str, db: Session):
+#     try:
+#         user = db.query(UserModel).filter(UserModel.email == email).first()
+
+#         if not user:
+#             return None
+
+#         if not Hash.verify(user.password, password):
+#             return None
+
+#         access_token = create_access_token(data={"sub": user.email})
+
+        
+#         return {
+#             "name": user.name,
+#             "email": user.email,
+#             "access_token": access_token
+#         }
+    
+#     except Exception as e:
+#         print("An exception occurred:", str(e))
+#         return None
+
+
+
+from sqlalchemy.orm import Session
+from app.hashing.password_hash import Hash
+from app.auth.jwt_handler import create_access_token
+from app.models.user_model import UserModel
+
+
+def login_user(email: str, password: str, db: Session):
+    try:
+        user = db.query(UserModel).filter(UserModel.email == email).first()
+
+        if not user or not Hash.verify(user.password, password):
+            return None
+
+        access_token = create_access_token(data={"sub": user.email})
+
+        return {
+            "name": user.name,
+            "email": user.email,
+            "access_token": access_token
+        }
+    
+    except Exception as e:
+        print("An exception occurred:", str(e))
+
