ab59af35b1b990b25ac661ad7a2d5eca79f65bdf
laxmiverve <laxminarayana.verve@gmail.com>
2024-08-20 14:05:35 +0530

    changes

--- app/schemas/company_update_schema.py
+++ app/schemas/company_update_schema.py
@@ -1,14 +1,6 @@
 from pydantic import BaseModel
 from typing import Optional
 
-# class CompanyUpdateSchema(BaseModel):
-#     company_name: Optional[str] = None
-#     company_email: Optional[str] = None
-#     company_number: Optional[str] = None
-#     company_zipcode: Optional[str] = None
-#     company_city: Optional[str] = None
-#     company_state: Optional[str] = None
-#     company_country: Optional[str] = None
 class CompanyUpdateSchema(BaseModel):
     company_name: Optional[str] 
     company_email: Optional[str] 
